fe79e2fc5fbba2eec56bd48db93f1cfebc70cbe1
pkonsomu2020 <pkonsomu2021@gmail.com>
2024-11-08 15:41:44 +0300

    DAY 5 CHALLENGE

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views  # Import views from the blog app
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),  # Point this to your homepage view
+    path('', views.homepage, name='homepage'),  # Homepage route
+    path('post/<int:id>/', views.post_detail, name='post_detail'),  # URL for individual post detail
 ]
--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
 # Create your views here.
 
 def homepage(request):
-    # Query all blog posts from the database
-    posts = BlogPost.objects.all()
+    posts = BlogPost.objects.all()  # Get all posts
     return render(request, 'blog/homepage.html', {'posts': posts})
+
+def post_detail(request, id):
+    # Use get_object_or_404 to retrieve a post by its id or return a 404 if not found
+    post = get_object_or_404(BlogPost, id=id)
+    return render(request, 'blog/post_detail.html', {'post': post})
