ff89235f66af1d187c5b4dee6744ee75d0066955
pkonsomu2020 <pkonsomu2021@gmail.com>
2024-11-08 21:24:38 +0300

    DAY 6 CHALLENGE

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -4,4 +4,6 @@ from . import views  # Import views from the blog app
 urlpatterns = [
     path('', views.homepage, name='homepage'),  # Homepage route
     path('post/<int:id>/', views.post_detail, name='post_detail'),  # URL for individual post detail
+    path('post/add/', views.add_post, name='add_post'),  # URL for adding a new post
+    path('post/edit/<int:id>/', views.edit_post, name='edit_post'),  # URL for editing a post
 ]
--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import BlogPost
+from .forms import BlogPostForm
 # Create your views here.
 
 def homepage(request):
@@ -10,3 +11,24 @@ def post_detail(request, id):
     # Use get_object_or_404 to retrieve a post by its id or return a 404 if not found
     post = get_object_or_404(BlogPost, id=id)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def add_post(request):
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the new post
+            return redirect('homepage')  # Redirect to the homepage
+    else:
+        form = BlogPostForm()
+    return render(request, 'blog/post_form.html', {'form': form, 'action': 'Add'})
+
+def edit_post(request, id):
+    post = get_object_or_404(BlogPost, id=id)
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()  # Update the existing post
+            return redirect('post_detail', id=post.id)  # Redirect to the post's detail page
+    else:
+        form = BlogPostForm(instance=post)
+    return render(request, 'blog/post_form.html', {'form': form, 'action': 'Edit'})
